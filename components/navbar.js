8f50d08889ad02f5ff08941822a97166451929c5
BANGATA <atanasiusditya@gmail.com>
2022-02-05 17:13:17 +0700

    Updating navbar
    Navbar done

--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,64 @@
 import {Flex, Text, Box, Button} from '@chakra-ui/react'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faUserCircle } from "@fortawesome/free-regular-svg-icons"
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <Flex>
-      
+    <Flex
+    w={'100%'}
+    // border={'solid'}
+    h={'90px'}
+    bgColor={'#161c27'}
+    >
+      <Flex 
+      // border={'solid'}
+      w={'15%'}
+      justifyContent={'center'}
+      alignItems={'center'}>
+          <Img src='/Logo_MXM.png'
+          bgColor={'white'}
+          borderRadius={50}
+          h={'75px'}/>
+      </Flex>
+      <Flex 
+      // border={'solid'}
+      w={'50%'}
+      color={'#fff'}
+      fontWeight={'bold'}
+      // justifyContent={'center'}
+      ml={10}
+      alignItems={'center'}
+      fontSize={20}>
+        <Flex>Nama Lengkap</Flex>
+        <Flex ml={20}>Divisi</Flex>  
+      </Flex>
+      <Flex 
+      // border={'solid'}
+      w={'20%'}
+      justifyContent={'center'}
+      alignItems={'center'}
+      ml={'400px'}
+      color={'#fff'}
+      >
+          <Link href=''><a>
+          <Button
+          h={50}
+          w={150}
+          borderRadius={50}
+          justifyContent={'center'}
+          fontSize={'18px'}
+          bg={'#1b4172'}
+          leftIcon={<FontAwesomeIcon icon={faUserCircle}/>}
+          mr={2}
+          fontWeight={'bold'}
+          fontSize={20}
+          >
+            Logout
+          </Button>
+          </a></Link>
+      </Flex>
     </Flex>
   );
 }
